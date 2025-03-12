8721d670d0974f60efe24df6f272b1c2fe3991e4
Saadaoui <122705066+Hazem322@users.noreply.github.com>
2025-03-12 22:47:33 +0100

    Update setup.py

--- deepfashion2_api/PythonAPI/setup.py
+++ deepfashion2_api/PythonAPI/setup.py
@@ -1,24 +1,16 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-import numpy as np
-
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 
 ext_modules = [
     Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs=[np.get_include(), '../common'],
-        extra_compile_args=[]  # Supprimer tous les arguments GCC incompatibles
-    )
+        name='pycocotools._mask',
+        sources=['pycocotools/_mask.pyx'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    ),
 ]
 
 setup(
     name='pycocotools',
-    packages=['pycocotools'],
-    package_dir={'pycocotools': 'pycocotools'},
-    version='2.0',
-    ext_modules=cythonize(ext_modules)
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
 )
